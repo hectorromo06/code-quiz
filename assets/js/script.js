3872d2a928892fc7a199186189e235ee5ea8ebda
Hector Romo <hectorommmo@gmail.com>
2022-04-17 20:36:25 -0700

    Logic for answer click

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,7 @@ var correctEl = document.querySelector('#correct');
 var startBtnEl = document.querySelector('#startBtn');
 
 var questionEl = document.querySelector('#question');
+var questionInd = 0;
 var answersEl = document.querySelector('#answers');
 
 var ans1 = document.createElement("button");
@@ -44,27 +45,47 @@ var ans2 = document.createElement("button");
 var ans3 = document.createElement("button");
 var ans4 = document.createElement("button");
 
+var scoresEl = document.querySelector('#scores');
+
 startBtnEl.addEventListener('click', function() {
     beginDivEl.setAttribute('class', 'uk-hidden');
     quizDivEl.setAttribute('class', 'uk-visible');
 
-    questionEl.textContent = questions[1].question;
+    questionEl.textContent = questions[questionInd].question;
 
-    ans1.textContent = questions[0].answers[0];
-    ans2.textContent = questions[0].answers[1];
-    ans3.textContent = questions[0].answers[2];
-    ans4.textContent = questions[0].answers[3];
+    ans1.textContent = questions[questionInd].answers[0];
+    ans2.textContent = questions[questionInd].answers[1];
+    ans3.textContent = questions[questionInd].answers[2];
+    ans4.textContent = questions[questionInd].answers[3];
 
     ans1.setAttribute('class', 'uk-button uk-button-primary');
     ans2.setAttribute('class', 'uk-button uk-button-primary');
     ans3.setAttribute('class', 'uk-button uk-button-primary');
     ans4.setAttribute('class', 'uk-button uk-button-primary');
 
+    ans1.setAttribute('data-number', questionInd);
+    ans2.setAttribute('data-number', questionInd);
+    ans3.setAttribute('data-number', questionInd);
+    ans4.setAttribute('data-number', questionInd);
+
     answersEl.appendChild(ans1);
     answersEl.appendChild(ans2);
     answersEl.appendChild(ans3);
     answersEl.appendChild(ans4);
+});
+
+answersEl.addEventListener('click', function(event) {
+    respDivEl.setAttribute('class', 'uk-visible');
+
+    var element = event.target;
+
+    var clicked = element.getAttribute('data-number');
     
-    // for (var i = 0; i < questions.length(); i++) {
-    // }
+    var ans = element.textContent;
+
+    if (questions[clicked].right == ans) {
+        correctEl.setAttribute('class', 'uk-visible');
+    } else if (questions[clicked].right != ans) {
+        wrongEl.setAttribute('class', 'uk-visible');
+    }
 });
